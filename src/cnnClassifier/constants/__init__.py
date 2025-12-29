080aedd49c69d168fb0aec6d67541c6a79420566
muggle <qnh11122006@gmail.com>
2025-12-29 11:38:28 +0700

    add

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,7 @@
+from src.cnnClassifier.constants import *
+from src.cnnClassifier.utils.common import read_yaml, create_directories
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
+SCHEMA_FILE_PATH = Path("schema.yaml")
