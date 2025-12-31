2f215c7b94a2b3e223609c1ff40856caebc88bc5
muggle <qnh11122006@gmail.com>
2025-12-31 11:03:34 +0700

    Data Validation & Data Ingestion completed

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 from src.cnnClassifier import logger
+from src.cnnClassifier.pipeline.data_ingestion_pipeline import DataIngestionPipeline
+from src.cnnClassifier.pipeline.data_validation_pipeline import DataValidationPipeline
 
-logger.info("Welcome to our custom logging")
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionPipeline()
+    obj.initiate_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationPipeline()
+    obj.initiate_data_validation()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/cnnClassifier/constants/__init__.py
+++ src/cnnClassifier/constants/__init__.py
@@ -1,5 +1,3 @@
-from src.cnnClassifier.constants import *
-from src.cnnClassifier.utils.common import read_yaml, create_directories
 from pathlib import Path
 
 CONFIG_FILE_PATH = Path("config/config.yaml")
